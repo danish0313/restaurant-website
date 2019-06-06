2078384e37a2e7f033419d9b958dc4671c16d82b
danish0313 <danishoo.dk@gmail.com>
2019-06-07 01:07:20 +0500

    Updates

---
+++ precache-manifest.a50170c1e9617985162098341be9a8b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a0149d9d0e952e702084c3c591cb38d",
+    "url": "/restaurant-website/static/media/logo.3a0149d9.png"
+  },
+  {
+    "revision": "95db12eb063c5d00aeef20d5cdd6800c",
+    "url": "/restaurant-website/static/media/background.95db12eb.jpg"
+  },
+  {
+    "revision": "bb5d1460768212b83f19",
+    "url": "/restaurant-website/static/js/runtime~main.bb5d1460.js"
+  },
+  {
+    "revision": "f527cb5890ea410a2e74",
+    "url": "/restaurant-website/static/js/main.f527cb58.chunk.js"
+  },
+  {
+    "revision": "7a38e549e7081876db02",
+    "url": "/restaurant-website/static/js/2.7a38e549.chunk.js"
+  },
+  {
+    "revision": "f527cb5890ea410a2e74",
+    "url": "/restaurant-website/static/css/main.0a4fe327.chunk.css"
+  },
+  {
+    "revision": "7a38e549e7081876db02",
+    "url": "/restaurant-website/static/css/2.0cae5bb2.chunk.css"
+  },
+  {
+    "revision": "d03ef668e0a56194b6ab02dbfe6f75d7",
+    "url": "/restaurant-website/index.html"
+  }
+];
