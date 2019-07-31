40398a791c87dc80e20517f7dc0a27a087c44cfd
danish0313 <danishoo.dk@gmail.com>
2019-08-01 01:00:43 +0500

    Updates

---
+++ precache-manifest.5dafe1aaf184e593acd685a5a8b2441c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a906828daa9884af48",
+    "url": "/restaurant-website/static/css/4.b57ab02a.chunk.css"
+  },
+  {
+    "revision": "e9f79d6080c532b1f283",
+    "url": "/restaurant-website/static/js/0.e9f79d60.chunk.js"
+  },
+  {
+    "revision": "db65f99393b5a703ab6b",
+    "url": "/restaurant-website/static/js/runtime~main.db65f993.js"
+  },
+  {
+    "revision": "95db12eb063c5d00aeef20d5cdd6800c",
+    "url": "/restaurant-website/static/media/background.95db12eb.jpg"
+  },
+  {
+    "revision": "d01994d19520ac974ff8",
+    "url": "/restaurant-website/static/js/3.d01994d1.chunk.js"
+  },
+  {
+    "revision": "0e36750494f41d64be7f",
+    "url": "/restaurant-website/static/js/main.0e367504.chunk.js"
+  },
+  {
+    "revision": "43a906828daa9884af48",
+    "url": "/restaurant-website/static/js/4.43a90682.chunk.js"
+  },
+  {
+    "revision": "024bf16ad1bb527a8752",
+    "url": "/restaurant-website/static/js/5.024bf16a.chunk.js"
+  },
+  {
+    "revision": "3a0149d9d0e952e702084c3c591cb38d",
+    "url": "/restaurant-website/static/media/logo.3a0149d9.png"
+  },
+  {
+    "revision": "d01994d19520ac974ff8",
+    "url": "/restaurant-website/static/css/3.901a8de3.chunk.css"
+  },
+  {
+    "revision": "b40210a1484de14558147d65be7c3039",
+    "url": "/restaurant-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41ddad21e4cde6e3d8b2286189ccc5b0.js"
+  "/restaurant-website/precache-manifest.5dafe1aaf184e593acd685a5a8b2441c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/restaurant-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
